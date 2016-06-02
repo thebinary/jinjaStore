5653c96fbd0f4df0e4021dc480dac386767deedd
thebinary <binary4bytes@gmail.com>
2016-06-02 19:36:55 +0545

    Add github URL in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name = 'jinjaStore',
         description = "Jinja Store for Templating",
         author = "The Binary",
         author_email = "binary4bytes@gmail.com",
-        url = "",
+        url = "https://github.com/thebinary/jinjaStore",
         packages = ['jinjaStore'],
         package_data = {
                 'jinjaStore': [ '*.jinja2' ]
